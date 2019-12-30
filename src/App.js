0a1b8d52732b12f178dbfc903f37da55c4571627
freef <mtfreeland@gmail.com>
2019-12-30 17:44:43 +0000

    refactor using context hooks

--- src/App.js
+++ src/App.js
@@ -14,12 +14,13 @@ import Blog from './Blog.js'
 import SideBar from './SideBar.js'
 import Footer from './Footer.js'
 import PrivacyPolicy from './PrivacyPolicy.js'
+import { ComicProvider } from './ComicContext.js'
 
 import Alert from 'react-bootstrap/Alert'
 import './index.scss'
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super()
     this.state = {
       user: null,
@@ -27,55 +28,57 @@ class App extends Component {
     }
   }
 
-    setUser = user => this.setState({ user })
-    clearUser = () => this.setState({ user: null })
-    alert = (message, type) => {
-      this.setState({ alerts: [...this.state.alerts, { message, type }] })
-    }
+  setUser = user => this.setState({ user })
+  clearUser = () => this.setState({ user: null })
+  alert = (message, type) => {
+    this.setState({ alerts: [...this.state.alerts, { message, type }] })
+  }
 
-    render () {
-      const { alerts, user } = this.state
-      return (
-        <React.Fragment>
+  render() {
+    const { alerts, user } = this.state
+    return (
+      <React.Fragment>
+        <ComicProvider>
           {alerts.map((alert, index) => (
             <Alert key={index} dismissible variant={alert.type}>
-              <Alert.Heading>
-                {alert.message}
-              </Alert.Heading>
+              <Alert.Heading>{alert.message}</Alert.Heading>
             </Alert>
           ))}
           <main className="container">
-            <Route path='/admin' render={() => (
-              <SignIn alert={this.alert} setUser={this.setUser} />
-            )} />
-            <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-              <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />
-            )} />
-            <AuthenticatedRoute user={user} path='/change-password' render={() => (
-              <ChangePassword alert={this.alert} user={user} />
-            )} />
-            <AuthenticatedRoute user={user} path='/upload' render={() => (<Uploader user={user} />)} />
-            <Route path='/' render={() => (<Nav />)} />
-            <Route exact path='/' component={Comic} />
-            <Route exact path='/comics/:id' component={Comic} />
-            <Route exact path='/feed' component={Rss} />
-            <Route exact path='/comics' component={Comic} />
-            <Route exact path='/privacypolicy' component={PrivacyPolicy} />
-            <Route path='/' component={HireMe} />
-            <section className='text-content'>
-              <Route exact path='/' component={Blog} />
-              <Route exact path='/' component={SideBar} />
-              <Route exact path='/blog/:id' component={Blog} />
-              <Route exact path='/blog/' component={Blog} />
-              <Route path='/blog' component={SideBar} />
+            <Route path="/admin" render={() => <SignIn alert={this.alert} setUser={this.setUser} />} />
+            <AuthenticatedRoute
+              user={user}
+              path="/sign-out"
+              render={() => <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />}
+            />
+            <AuthenticatedRoute
+              user={user}
+              path="/change-password"
+              render={() => <ChangePassword alert={this.alert} user={user} />}
+            />
+            <AuthenticatedRoute user={user} path="/upload" render={() => <Uploader user={user} />} />
+            <Route path="/" render={() => <Nav />} />
+            <Route exact path="/" component={Comic} />
+            <Route exact path="/comics/:id" component={Comic} />
+            <Route exact path="/feed" component={Rss} />
+            <Route exact path="/comics" component={Comic} />
+            <Route exact path="/privacypolicy" component={PrivacyPolicy} />
+            <Route path="/" component={HireMe} />
+            <section className="text-content">
+              <Route exact path="/" component={Blog} />
+              <Route exact path="/" component={SideBar} />
+              <Route exact path="/blog/:id" component={Blog} />
+              <Route exact path="/blog/" component={Blog} />
+              <Route path="/blog" component={SideBar} />
             </section>
             <section>
-              <Route path='/' component={Footer} />
+              <Route path="/" component={Footer} />
             </section>
           </main>
-        </React.Fragment>
-      )
-    }
+        </ComicProvider>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
